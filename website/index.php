1965856656e1061cda06c3e209a1d548d9eac63a
Nate Holcomb <nholcom@bgsu.edu>
2023-02-06 05:33:03 -0500

    add files for website

--- website/index.php
+++ website/index.php
@@ -1,19 +1,53 @@
 <!DOCTYPE html>
-<html>
-    <!-- Metadata -->
-    <head>
-        <meta charset = "utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> welcome to the code zone B) | Nate Holcomb </title>
+<html lang="en">
+<!-- portfolio written by Nate Holcomb, Feb. 5th, 2023 --> 
 
+<!--Description: index.php - welcome to the code zone B) | Nate Holcomb -->
+<head>
+	<title> welcome to the code zone B) | Nate Holcomb </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- BOOTSTRAP LINK, GOOGLE API LINK, JQUERY/JS LINK-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<!-- end linking. if you want to add more files, declarations go below -->
+</head>
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel = "stylesheet" href="css/style.css">
-    </head>
+<!--MAKING THE MAIN CONTAINER-->
+<div class="container-fluid no-pad">
 
-    <!-- Begin body -->
-    <body>
+	<!-- making the navbar -->
+	<div class="header">
+		<a href="index.php">
+			<img id="logo" src="https://cdn.discordapp.com/attachments/1072009704311635998/1072011008652099645/index.png" alt="nholcom">
+		</a>
+		<div class="topnav">
+      <a href="works.php">Coding Examples</a>
+			<a href="about.php">About</a>
+      <a href="index.php">Home</a>
+		</div>
+	</div>
 
-    </body>
+<!--            begin main              -->
+	<div class = "jumbotron text-center">
+		<h1>welcome to the code zone 😎</h1>
+		<br>
+		<ul>
+      <li>BGSU graduate with a Bachelor's in CS</li>
+      <li>4+ years of experience with C++, Python, HTML, CSS</li>
+      <li>I made this portfolio, which is pretty neat</li>
+    </ul>
+	</div>	
+	<!--            end main              -->
+  
+	<footer class="footer text-center">
+		<div class = "container">
+			<span class = "text-muted"> Copyright 2023 Nate Holcomb. All rights reserved.</span>
+		</div>
+	</footer>
+	
+</div>
 
 </html>

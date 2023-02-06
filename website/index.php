bd656464f7d52f6e052e88e3fc883afab330b439
Nate Holcomb <nholcom@bgsu.edu>
2023-02-06 05:13:32 -0500

    begin work on portfolio

---
+++ website/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <!-- Metadata -->
+    <head>
+        <meta charset = "utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> welcome to the code zone B) | Nate Holcomb </title>
+
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel = "stylesheet" href="css/style.css">
+    </head>
+
+    <!-- Begin body -->
+    <body>
+
+    </body>
+
+</html>
